e692136ce275e28aeacc0b569939ce0e2d812457
DryRun <david.renhwa.yu@gmail.com>
2020-11-09 23:26:22 -0600

    Add fit all data scripts

---
+++ barista/fitting/fitalldata.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python fitdata_Bs2KKMuMu.py --all --fits --plots --tables
+python fitdata_Bu2KMuMu.py --all --fits --plots --tables
+python fitdata_Bd2KPiMuMu.py --all --fits --plots --tables
---
+++ barista/fitting/fitalldatabinned.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python fitdata_Bs2KKMuMu.py --all --fits --plots --tables --binned
+python fitdata_Bu2KMuMu.py --all --fits --plots --tables --binned
+python fitdata_Bd2KPiMuMu.py --all --fits --plots --tables --binned
